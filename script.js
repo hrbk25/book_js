8cea2273e3825de865f5028f4e43f8162c4b5f83
hrbk25 <hrbk25@gmail.com>
2022-10-10 22:01:01 +0900

    study javaScript by book(Day12)

--- script.js
+++ script.js
@@ -11,9 +11,14 @@ function fail(error) {
 
 navigator.geolocation.getCurrentPosition(success, fail);
 
+// UTCをミリ秒に
+function utxToJSTime(utcTime) {
+    return utcTime * 1000;
+}
+
 // データ取得
 function ajaxRequest(lat, long) {
-    const url ='https://aou.openweathermap.org/data/2.5/forecast';
+    const url ='https://api.openweathermap.org/data/2.5/forecast';
     const appId = '7c6d4cf22b933d83e56beb4bfa0d8678';
 }
 
@@ -29,6 +34,27 @@ $.ajax({
 })
 .done(function(data) {
     console.log(data);
+
+    // 都市名、国名
+    console.log('都市名：' + data.city.name);
+    console.log('国名：' + data.city.country);
+
+    // 天気予報データ
+    data.list.forEach(function(forecast, index){
+        const dateTime = new Date(utcToJSTime(forecast.dt));
+        const month = dateTime.getMonth() + 1;
+        const date = dateTime.getDate();
+        const hours = dateTime.getHours();
+        const min = String(dateTime.getMinutes()).padStart(2, '0');
+        const temperature = Math.round(forecast.main.temp);
+        const description = forecast.weather[0].description;
+        const iconPath = `images/${forecast.weather[0].icon}.svg`;
+
+        console.log('日時：' + `${month}/${date} ${hours}:${min}`);
+        console.log('気温：' + temperature);
+        console.log('天気：' + description);
+        console.log('画像パス：' + iconPath);
+    });
 })
 .fail(function() {
     console.log('$.ajax failed!');
